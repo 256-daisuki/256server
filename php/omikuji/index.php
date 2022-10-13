552fb96616dab6301878f3c994de73beaf5bc011
256-daisuki <256mitdaisuki+git@gmail.com>
2022-10-14 00:29:33 +0900

    完成形

--- php/omikuji/index.php
+++ php/omikuji/index.php
@@ -3,8 +3,8 @@
 // 大吉　中吉　小吉　末吉　凶
 $omikuji = rand(1,8);
 if($omikuji == 1) {
-    echo "大吉";
-    } elseif ($omikuji == 2) {
+    echo "大吉"; // 1を大吉に
+    } elseif ($omikuji == 2) { // 2を中吉に
         echo "中吉";
     } elseif ($omikuji <= 4) { // 3,4を小吉に
         echo "小吉";
