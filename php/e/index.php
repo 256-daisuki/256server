dc1ed2117cb0b03bf8b0282ff59b66ebbedbca51
256-daisuki <256mitdaisuki+git@gmail.com>
2022-10-14 19:44:51 +0900

    最終更新

--- php/e/index.php
+++ php/e/index.php
@@ -1,7 +1,3 @@
-<?php
-$num = $_GET["256"];
-echo (1 + ( 1 / $num))**$num;
-?>
 <!DOCTYPE html>
 <html lang="ja" dir="ltr">
   <head>
@@ -9,9 +5,15 @@ echo (1 + ( 1 / $num))**$num;
     <title>e(ネイピア数)の計算</title>
   </head>
   <body>
+    <h1>ネイピア数を計算するよ</h1>
+    <h2>下に数字を入力してね（大きければ大きいほど正確になるよ）</h2>
     <form action="/php/e/index.php" method="get">
-      <input type="text" name="256"><br>
+      <input type="text" name="256" value="0"><br>
       <input type="submit" value="送信">
     </form>
+    <?php
+    $num = $_GET["256"];
+    echo (1 + ( 1 / $num))**$num;
+  ?>
   </body>
 </html>
