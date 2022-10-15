044a757019636300a67d5c6452be44a59aa0d068
256-daisuki <256mitdaisuki+git@gmail.com>
2022-10-15 21:48:22 +0900

    初期版

---
+++ php/10to2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>10進数を2進数にするよ</title>
+  </head>
+  <body>
+    <h1>10進数を2進数と16進数にするよ</h1>
+    <h2>下に変換する数字を入力してね</h2>
+    <form action="/php/10to2/index.php" method="get">
+      <input type="text" name="num" value="0"><br>
+      <input type="submit" value="変換">
+    </form>
+    <?php
+    $num = $_GET["num"];
+    echo $num . "は2進数で" . decbin($num);
+    echo "<br/>";
+    echo $num . "は10進数で" . dechex($num);
+    ?>
+  </body>
+</html>
