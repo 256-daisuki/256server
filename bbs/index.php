48c70090f63475d81f7756c70762be1e747f4719
256-daisuki <256mitdaisuki+git@gmail.com>
2022-10-17 22:39:54 +0900

    ひな形の完成

--- bbs/index.php
+++ bbs/index.php
@@ -1,3 +1,7 @@
+<?php
+    echo $_POST["username"];
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -11,9 +15,27 @@
     <h1 class="title">256ch</h1>
     <hr>
     <div class="boardWrapper">
+        <section>
+            <article>
+                <div class="wrapper">
+                    <div class="nameArea">
+                        <span>名前:</span>
+                        <p class="username">256daisuke</p>
+                        <time>:2022/10/17</time>
+                    </div>
+                    <p class="comment">hello world</p>
+                </div>
+            </article>
+        </section>
         <form class="formWrapper">
             <div>
                 <input type="submit" value="書き込む">
+                <label for="">名前:</label>
+                <input type="text" name="username">
+            </div>
+            <div>
+                <textarea class="commentTextArea"></textarea>
+            </div>
             </div>
         </form>
     </div>
