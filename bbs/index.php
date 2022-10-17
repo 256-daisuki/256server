d53d55d20d4dc3a2479ce80c9eb2c301f716af0d
256-daisuki <256mitdaisuki+git@gmail.com>
2022-10-17 22:18:07 +0900

    HTMLtest

--- bbs/index.php
+++ bbs/index.php
@@ -1,3 +1,16 @@
 <?php
 echo "hello php";
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>256ch</title>
+</head>
+<body>
+    
+</body>
+</html>
