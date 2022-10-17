ba7dd2c890085178767860c1ad931078bc57208b
256-daisuki <256mitdaisuki+git@gmail.com>
2022-10-17 22:23:58 +0900

    おいしいバナナ

--- bbs/index.php
+++ bbs/index.php
@@ -1,7 +1,3 @@
-<?php
-echo "hello php";
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -9,8 +5,17 @@ echo "hello php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>256ch</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1 class="title">256ch</h1>
+    <hr>
+    <div class="boardWrapper">
+        <form class="formWrapper">
+            <div>
+                <input type="submit" value="書き込む">
+            </div>
+        </form>
+    </div>
 </body>
 </html>
