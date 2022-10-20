59dd32bd58696ce63cddbfe129ccb1cfdf822a43
256-daisuki <256mitdaisuki+git@gmail.com>
2022-10-20 20:51:30 +0900

    もう修正したくない！！！！

--- bbs/dbconnect.php
+++ bbs/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO ('mysql:dbname=bbs;host=192.168.3.13; charset=utf8', 'root', '');
+    $db = new PDO ('mysql:dbname=bbs;host=localhost; charset=utf8', 'root', '');
 } catch (PDOException $e) {
     echo 'DB接続エラー' . $e->getMessage;
 }
