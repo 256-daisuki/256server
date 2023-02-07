24d76b5439ebe64e820d6f2570b406ce0a4d7e8a
256-daisuki <256mitdaisuki+git@gmail.com>
2023-02-08 05:15:17 +0900

    いつ書いたっけ

--- todo/main.js
+++ todo/main.js
@@ -1,108 +1,41 @@
-// クラス
-class TodoList {
-    // コンストラクタ
-    constructor() {
-            this.DOM = {};
-            this.DOM.incomplete = document.querySelector(".item-area-incomplete ul");
-            this.DOM.complete = document.querySelector(".item-area-complete ul");
+import "./styles.css";
 
-        }
-        // リストを生成
-    _createItem(text) {
-            const liElm = document.createElement('li');
-            const pElm = document.createElement('p');
-            const deleteButton = document.createElement('button');
-            const completeButton = document.createElement('button');
+const onClickAdd = () => {
+  // 入力値を取得する
+  const inputText = document.getElementById("add-text").value;
+  // フォームを空にする
+  document.getElementById("add-text").value = "";
 
-            completeButton.classList.add('complete-button');
-            completeButton.innerText = '完了';
-            deleteButton.classList.add('incomplete-button');
-            deleteButton.innerText = '削除';
-            pElm.innerText = text;
+  // divを生成
+  const div = document.createElement("div");
+  console.log(div);
+  // <div></div>
 
-            // 完了ボタンクリック
-            completeButton.addEventListener('click', (e) => {
-                this.deleteItem(e.srcElement.parentNode, this.DOM.incomplete);
-                this.addCompleteItem(text);
-            });
-            // 削除ボタンクリック
-            deleteButton.addEventListener('click', (e) => {
-                const confirmation = confirm("本当に消しますか？");
+  // クラス名を追加
+  div.className = "list-row";
+  console.log(div);
+  // <div class="list-row"></div>
 
-                if (confirmation) {
-                    this.deleteItem(e.srcElement.parentNode, this.DOM.incomplete);
-                }
-            });
+  // liタグ生成
+  const li = document.createElement("li");
+  console.log(li);
+  // <li></li>
+  // liの中にinputTextの内容を取得
+  li.innerText = inputText;
+  // <li>テスト</li>
 
-            // 生成した要素
-            liElm.appendChild(pElm);
-            liElm.appendChild(completeButton);
-            liElm.appendChild(deleteButton);
+  // divの子要素に各要素を設定する
+  div.appendChild(li);
+  /*
+    <div class="list-row">
+      <li>テスト</li>
+    </div>
+  */
 
-            return liElm;
-        }
-        // 完了に追加
-    _completeItem(text) {
-            const liElm = document.createElement('li');
-            const pElm = document.createElement('p');
-            const backButton = document.createElement('button');
+  // 未完了リストにタスクを追加
+  document.getElementById("incomplete-list").appendChild(div);
+  // 未完了リストの一番下にフォームに入力した内容が表示される
+};
 
-            backButton.classList.add('complete-button');
-            backButton.innerText = '戻る';
-            pElm.innerText = text;
-            // 戻るボタンクリック
-            backButton.addEventListener('click', (e) => {
-                this.deleteItem(e.srcElement.parentNode, this.DOM.complete);
-                this.addItem(text);
-            });
-
-            liElm.appendChild(pElm);
-            liElm.appendChild(backButton);
-
-            return liElm;
-        }
-        // リストを追加
-    addItem(text) {
-            this.DOM.incomplete.appendChild(this._createItem(text));
-        }
-        // 完了に追加
-    addCompleteItem(text) {
-            this.DOM.complete.appendChild(this._completeItem(text));
-        }
-        // リストを削除
-    deleteItem(target, domparent) {
-        domparent.removeChild(target);
-    }
-}
-
-function addtodoEvent() {
-    const addItemTxt = document.querySelector(".add-item").value;
-    // 値を入力していない時は処理を終了
-    if (addItemTxt == "") {
-        alert("値を入力してください");
-        return
-    }
-    // フォームの値をリセット
-    document.querySelector(".add-item").value = "";
-    // インスタンス化
-    const totoList = new TodoList();
-    totoList.addItem(addItemTxt);
-}
-
-// クリックイベント
-document.querySelector(".add-button").addEventListener('click', () => {
-    addtodoEvent();
-});
-
-// キーボードイベント
-document.addEventListener('keypress', keypress);
-const regexEnter = new RegExp('(=|Enter)');
-
-function keypress(e) {
-    // エンターが押されたら
-    if (regexEnter.test(e.key)) {
-        addtodoEvent();
-    } else {
-        return false;
-    }
-}
+// 追加ボタンをクリックした際にonClickAddを実行
+document.getElementById("add-button").addEventListener("click", () => onClickAdd());
