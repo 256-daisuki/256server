919adf569c4ee85c155c23eddcfb88fb6ac4fa00
256-daisuki <256mitdaisuki+git@gmail.com>
2023-02-22 23:03:26 +0900

    ファイル移動

---
+++ gen4/AI/test1/main.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const osUtils = require('os-utils');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('a.html', (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.write('Not Found!');
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+      }
+      res.end();
+    });
+  } else {
+    res.writeHead(404);
+    res.write('Not Found!');
+    res.end();
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+
+  setInterval(() => {
+    osUtils.cpuUsage((cpuUsage) => {
+      const totalMem = osUtils.totalmem();
+      const freeMem = osUtils.freemem();
+      const memUsage = ((totalMem - freeMem) / totalMem) * 100;
+
+      const html = `<p>CPU: ${Math.floor(cpuUsage.toFixed(2) * 100) }%</p><p>MEM: ${memUsage.toFixed(2)}%</p>`;
+      fs.writeFile('a.html', html, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
+  }, 1000);
+});
---
+++ gen4/php/omikuji/index.php
@@ -0,0 +1,16 @@
+<?php
+// おみくじを作る
+// 大吉　中吉　小吉　末吉　凶
+$omikuji = rand(1,8);
+if($omikuji == 1) {
+    echo "大吉"; // 1を大吉に
+    } elseif ($omikuji == 2) { // 2を中吉に
+        echo "中吉";
+    } elseif ($omikuji <= 4) { // 3,4を小吉に
+        echo "小吉";
+    } elseif ($omikuji <= 7) { // 5,6,7を末吉に
+        echo "末吉";
+    } else {
+        echo "凶"; // 8を凶に
+    }
+?>
