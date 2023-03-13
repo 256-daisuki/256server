d417c90b033473da7a43b33f63812439cc56e95b
256-daisuki <256mitdaisuki+git@gmail.com>
2023-03-14 05:56:14 +0900

    AIにコードぐちゃぐちゃにされたわ 怒っていません

--- new/bbs/form.php
+++ new/bbs/form.php
@@ -1,6 +1,22 @@
 <form method="post" action="write.php">
-    名前：<input type="text" name="name"><br>
+    名前：<input type="text" name="name" id="name"><br>
     本文：<br>
-    <textarea name="body" cols="40" rows="5"></textarea><br>
-    <input type="submit" value="書き込む">
+    <textarea name="body" id="body" cols="40" rows="5"></textarea><br>
+    <input type="submit" value="書き込む" onclick="checkForm()">
 </form>
+
+<script>
+function checkForm() {
+  var nameField = document.getElementById("name");
+  var bodyField = document.getElementById("body");
+
+  if (nameField.value == "") {
+    nameField.value = "名無し";
+  }
+
+  if (bodyField.value.trim() == "") {
+    alert("本文を入力してください。");
+    event.preventDefault();
+  }
+}
+</script>
