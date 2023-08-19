a719a4731f39f191eb1202a6a7ce6f2d639d0118
256-daisuki <256mitdaisuki+git@gmail.com>
2023-08-20 02:11:55 +0900

    na

--- discord-bot/test.py
+++ discord-bot/test.py
@@ -1,50 +1,27 @@
 import discord
-from discord.ext import commands
-import numpy as np
-import matplotlib
-matplotlib.use('agg')  # バックエンドを切り替える
-import matplotlib.pyplot as plt
 
+# インテントの生成
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
+intents.message_content = True
 
-@bot.event
+# クライアントの生成
+client = discord.Client(intents=intents)
+
+# discordと接続した時に呼ばれる
+@client.event
 async def on_ready():
-    print("Bot is ready")
+    print(f'We have logged in as {client.user}')
 
-@bot.command(name="polygon", description="n角形の図形を描画します")
-async def polygon_command(ctx, n: int):
-    angle = 360 / n
+# メッセージを受信した時に呼ばれる
+@client.event
+async def on_message(message):
+    # 自分のメッセージを無効
+    if message.author == client.user:
+        return
 
-    vertices = []
-    for i in range(n):
-        theta = np.radians(i * angle)
-        x = np.cos(theta)
-        y = np.sin(theta)
-        vertices.append((x, y))
+    # メッセージが"$hello"で始まっていたら"Hello!"と応答
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-    fig, ax = plt.subplots()
-
-    for i in range(n):
-        ax.plot([vertices[i][0], vertices[(i + 1) % n][0]],
-                [vertices[i][1], vertices[(i + 1) % n][1]], color='b')
-
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_aspect('equal')
-    ax.set_facecolor((0, 0, 0, 0))
-    ax.spines['top'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.xaxis.set_ticks_position('none')
-    ax.yaxis.set_ticks_position('none')
-    ax.grid(False)
-
-    plt.savefig("transparent_polygon_no_labels.png", transparent=True, bbox_inches='tight', pad_inches=0)
-
-    with open("transparent_polygon_no_labels.png", "rb") as file:
-        picture = discord.File(file)
-        await ctx.send(file=picture)
-
-bot.run('MTE0MTg4NjQxNzYxODg4MjYyMA.G7IRxa.ZADUXXpEh-9uD5yBUAMJwNllLCZ01TfK5LL-vk')
+# クライアントの実行
+client.run('MTE0MTg4NjQxNzYxODg4MjYyMA.G7IRxa.ZADUXXpEh-9uD5yBUAMJwNllLCZ01TfK5LL-vk')
