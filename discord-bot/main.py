fc62dd031516c76111af15d2075d86e0d37117ff
256-daisuki <256mitdaisuki+git@gmail.com>
2023-08-20 02:01:30 +0900

    discord

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -1,6 +1,9 @@
 import discord
 import random
+import requests
 from discord import app_commands
+from bs4 import BeautifulSoup
+import subprocess
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -24,13 +27,40 @@ async def ping_command(interaction: discord.Interaction):
     ping = round(raw_ping * 1000)
 
     # 送信する
-    embed = discord.Embed(title="Pong!🏓", description=f"BotのPing値は{ping}msです。")
+    embed = discord.Embed(
+        title="Pong!🏓", 
+        color=0x00ff00,
+        description=f"BotのPing値は{ping}msです。")
     await interaction.response.send_message(embed=embed)
 
 @tree.command(name="omikuji", description="おみくじ　そのまま")
 async def ping_command(interaction: discord.Interaction):
     omikuji = ["凶", "大凶"]
     await interaction.response.send_message(f"今日のお前の運勢 {random.choice(omikuji)}")
+
+@tree.command(name="echo", description="あんなことやそんなことまで言います")
+async def echo_command(interaction: discord.Interaction, *, text: str):
+    await interaction.response.send_message(text, ephemeral=False)
+
+@tree.command(name="google", description="Googleで検索結果を表示します")
+async def google_command(interaction: discord.Interaction, *, search_word: str):
+    pages_num = 10 + 1  # 上位から何件までのサイトを抽出するか指定
+    result_embed = discord.Embed(title=f"Google検索結果: {search_word}", color=0x3498db)
+
+    url = f'https://www.google.co.jp/search?hl=ja&num={pages_num}&q={search_word}'
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, "html.parser")
+    search_site_list = soup.select('div.kCrYT > a')
+
+    for site in search_site_list:
+        try:
+            site_title = site.select('h3.zBAuLc')[0].text
+        except IndexError:
+            site_title = site.select('img')[0]['alt']
+        site_url = site['href'].replace('/url?q=', '')
+        result_embed.add_field(name="\u200B", value=f"[{site_title}]({site_url})", inline=False)  # 空白テキストを使ってタイトルを表示しないように
+
+    await interaction.response.send_message(embed=result_embed, ephemeral=False)
 
 # メッセージを受信した時に呼ばれる
 @client.event
@@ -39,5 +69,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith('a'):
+        await message.channel.send('Hello!')
+
 # トークン
-client.run('ここに貴方のトークンを配置')
+client.run('')
