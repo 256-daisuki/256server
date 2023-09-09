88ac8bb19938d497dd4012efc418909b9e20dc85
256-daisuki <256mitdaisuki+git@gmail.com>
2023-09-10 05:11:32 +0900

    コードの簡素化

--- email/main.js
+++ email/main.js
@@ -3,16 +3,22 @@ require('dotenv').config(); // .envファイルの読み込み
 const axios = require('axios');
 
 const url = "https://api.xrea.com/v1/user/info";
-const payload = new URLSearchParams();
 
-payload.append('api_secret_key', process.env.API_SECRET_KEY);
-payload.append('account', process.env.ACCOUNT);
-payload.append('server_name', process.env.SERVER_NAME);
+const data = {
+    account: process.env.ACCOUNT,
+    server_name: process.env.SERVER_NAME,
+    api_secret_key: process.env.API_SECRET_KEY
+};
 
-axios.post(url, payload)
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error(error);
+axios.post(url, data, {
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded', // フォームデータを指定
+    }
+})
+.then(response => {
+    console.log(response.data);
+})
+.catch(error => {
+    console.error(error);
 });
+
