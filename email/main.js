69b2d7a6c5ab1a9a23857bb3321a5e378661d305
256-daisuki <256mitdaisuki+git@gmail.com>
2023-09-10 05:09:09 +0900

    connectの成功

--- email/main.js
+++ email/main.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 
 const url = "https://api.xrea.com/v1/user/info";
 const payload = new URLSearchParams();
+
 payload.append('api_secret_key', process.env.API_SECRET_KEY);
 payload.append('account', process.env.ACCOUNT);
 payload.append('server_name', process.env.SERVER_NAME);
