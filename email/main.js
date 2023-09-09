b7448055455d1b354db1649b208bbe86116a66ed
256-daisuki <256mitdaisuki+git@gmail.com>
2023-09-09 23:35:24 +0900

    ぬぬぬぬぬぬぬぬぬぬぬぬぬぬ

--- email/main.js
+++ email/main.js
@@ -1,12 +1,17 @@
+require('dotenv').config(); // .envファイルの読み込み
+
 const axios = require('axios');
 
 const url = "https://api.xrea.com/v1/user/info";
-const payload = { account: "n256" , server_name: "m4.xrea.com" , api_secret_key: "PBGuaXAozsGdgTULQeLmDzdoEDAWrUAG" }; // ここに実際のAPIキーを追加
+const payload = new URLSearchParams();
+payload.append('api_secret_key', process.env.API_SECRET_KEY);
+payload.append('account', process.env.ACCOUNT);
+payload.append('server_name', process.env.SERVER_NAME);
 
 axios.post(url, payload)
     .then(response => {
-    console.log(response.data);
+        console.log(response.data);
     })
     .catch(error => {
-    console.error(error);
-    });
+        console.error(error);
+});
