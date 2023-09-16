8b5c7e324eb0c15b7e15cedb8bd563c6c59cdcee
256-daisuki <256mitdaisuki+git@gmail.com>
2023-09-17 01:28:32 +0900

    ぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬぬ

--- test/script.js
+++
@@ -1,9 +0,0 @@
-const ham = document.querySelector('#js-hamburger');
-const nav = document.querySelector('#js-nav');
-
-ham.addEventListener('click', function () {
-
-  ham.classList.toggle('active');
-  nav.classList.toggle('active');
-
-});
