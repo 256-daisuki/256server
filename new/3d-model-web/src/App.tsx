27a095c31db0ed5a2429545f2f0d7e6dd1f46ca5
256-daisuki <256mitdaisuki+git@gmail.com>
2023-10-06 04:09:06 +0900

    なんか惜しい

--- new/3d-model-web/src/App.tsx
+++ new/3d-model-web/src/App.tsx
@@ -6,6 +6,11 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 function App() {
   let canvas: HTMLCanvasElement;
   let model: THREE.Group;
+  let isDragging = false;
+  let previousMousePosition = {
+    x: 0,
+    y: 0,
+  };
 
   useEffect(() => {
     canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -13,60 +18,137 @@ function App() {
     const sizes = {
       width: innerWidth,
       height: innerHeight,
-    }
-    //scene
+    };
+    // scene
     const scene: THREE.Scene = new THREE.Scene();
 
-    //camera
+    // camera
     const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
-      75, 
+      75,
       sizes.width / sizes.height,
       0.1,
       1000
-      );
-      camera.position.set(0, 0.2, 2);
+    );
+    camera.position.set(0, 0.2, 2);
 
-    //renderer
+    // renderer
     const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
       canvas: canvas,
       antialias: true,
-      alpha: true, //透明度を取る
+      alpha: true,
     });
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(window.devicePixelRatio);
 
-    // const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
-    // const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
-    // const cube = new THREE.Mesh( geometry, material ); 
-    // scene.add( cube );
-
-    //3dモデルインポート
+    // 3dモデルインポート
     const gltfLoader = new GLTFLoader();
 
     gltfLoader.load("./models/scene.gltf", (gltf) => {
       model = gltf.scene;
-      model.scale.set(0.7, 0.7, 0.7)
+      model.scale.set(0.7, 0.7, 0.7);
       model.rotation.y = -Math.PI / 8;
 
       scene.add(model);
     });
 
-    //アニメーション
+    // マウスの座標を保持する変数
+    const mouse = new THREE.Vector2();
+
+    // マウスのドラッグ開始
+    canvas.addEventListener("mousedown", (event) => {
+      isDragging = true;
+      previousMousePosition.x = event.clientX;
+      previousMousePosition.y = event.clientY;
+    });
+
+    // マウスの移動に応じてモデルを回転させる
+    canvas.addEventListener("mousemove", (event) => {
+      // ドラッグ中のみ回転
+      if (isDragging) {
+        const deltaX = event.clientX - previousMousePosition.x;
+        const deltaY = event.clientY - previousMousePosition.y;
+
+        // マウスの移動に応じてモデルを回転
+        model.rotation.x += deltaY * 0.01;
+        model.rotation.y += deltaX * 0.01;
+      }
+
+      previousMousePosition.x = event.clientX;
+      previousMousePosition.y = event.clientY;
+    });
+
+    // マウスのドラッグ終了
+    canvas.addEventListener("mouseup", () => {
+      isDragging = false;
+    });
+
+    // マウスのホイールイベント
+    canvas.addEventListener("wheel", (event) => {
+      const zoomSpeed = 0.005;
+      const zoom = 1 + event.deltaY * zoomSpeed;
+
+      // マウスの位置を取得
+      const mouseX = (event.clientX / sizes.width) * 2 - 1;
+      const mouseY = -((event.clientY / sizes.height) * 2 - 1);
+
+      // マウスの位置を中心に拡大縮小
+      camera.position.x = camera.position.x * zoom + mouseX;
+      camera.position.y = camera.position.y * zoom + mouseY;
+      camera.position.z *= zoom;
+
+      // カメラ位置を固定
+      camera.lookAt(mouseX, mouseY, 0); // マウスポインターの位置を注視点に設定
+    });
+
+    // マウスの移動に応じてモデルを回転させる
+    canvas.addEventListener("mousemove", (event) => {
+      // マウス座標を正規化
+      mouse.x = (event.clientX / sizes.width) * 2 - 1;
+      mouse.y = -((event.clientY / sizes.height) * 2 - 1);
+
+      // ドラッグ中のみ回転
+      if (isDragging) {
+        const deltaX = event.clientX - previousMousePosition.x;
+        const deltaY = event.clientY - previousMousePosition.y;
+
+        // マウスの移動に応じてモデルを回転
+        model.rotation.x += deltaY * 0.01;
+        model.rotation.y += deltaX * 0.01;
+      }
+
+      previousMousePosition.x = event.clientX;
+      previousMousePosition.y = event.clientY;
+    });
+
+    // マウスのホイールイベント
+    canvas.addEventListener("wheel", (event) => {
+      const zoomSpeed = 0.0005;
+      const zoom = 1 + event.deltaY * zoomSpeed;
+
+      // マウスの位置を中心に拡大縮小
+      camera.position.x *= zoom;
+      camera.position.y *= zoom;
+      camera.position.z *= zoom;
+
+      event.preventDefault(); // デフォルトのスクロールを防止
+    });
+
+    // アニメーション
     function tick() {
       renderer.render(scene, camera);
       requestAnimationFrame(tick);
     }
     tick();
-  },[]);
+  }, []);
 
   return (
     <>
       <canvas id="canvas"></canvas>
-      <div className='mainContent'>
+      <div className="mainContent">
         <h3>hello world</h3>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
